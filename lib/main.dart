aaff384977a011a1dfa581a768e5702d50e5dea2
garg-akanksha <44610289+garg-akanksha@users.noreply.github.com>
2020-11-27 15:25:57 +0530

    signup page code added

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,63 @@ class Home  extends StatelessWidget {
         title: Text('Signup Page'),
         centerTitle: true, 
       ),
+      body: 
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0,30.0,20.0,30.0),
+            child: Container(
+                child: Icon(
+                  Icons.account_circle,
+                  size: 160.0, 
+                ),
+
+              ),
+          ),
+            // SizedBox(
+            //     height: 30.0,
+
+            //   ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0,8.0, 20.0,8.0),
+            child:
+              TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'UserName',
+             
+              ),
+              ),
+              ),
+              SizedBox(
+                height: 30.0,
+
+              ),
+               Padding(
+                 padding: const EdgeInsets.fromLTRB(20.0, 8.0,20.0, 8.0),
+                 child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Password',
+             
+              ),
+              ),
+               ),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: FloatingActionButton.extended(
+                  onPressed: (){},
+                  label: Text('Signup'),
+                  backgroundColor: Colors.amber,
+            
+                 ),
+              ),          
+        ],
+      ),
       
     );
   }
